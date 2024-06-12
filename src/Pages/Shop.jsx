f674506ffcb9d2e741933cb4cdd6fac2eeae9684
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-06-12 13:30:24 +0600

    News Letter Section Done

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import { Offers } from "../Components/Offers/Offers"
 import {Section} from "../Components/Section/Section"
 import data_product from '../Components/Assets/data'
 import new_collection from '../Components/Assets/new_collections'
+import { NewsLetter } from "../Components/NewsLetter/NewsLetter"
 
 
 export const Shop = () => {
@@ -12,6 +13,7 @@ export const Shop = () => {
       <Section title="POPULAR IN WOMEN" data={data_product}/>
       <Offers/>
       <Section title="NEW COLLECTIONS" data={new_collection}/>
+      <NewsLetter/>
     </div>
   )
 }
