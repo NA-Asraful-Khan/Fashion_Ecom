6b6b46e0ae178ad3078441ca8084c7021c69dd57
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-06-12 11:44:29 +0600

    Offer Section done

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,4 +1,5 @@
 import { Hero } from "../Components/Hero/Hero"
+import { Offers } from "../Components/Offers/Offers"
 import { Popular } from "../Components/Popular.jsx/Popular"
 
 export const Shop = () => {
@@ -6,6 +7,7 @@ export const Shop = () => {
     <div>
       <Hero/>
       <Popular/>
+      <Offers/>
     </div>
   )
 }
