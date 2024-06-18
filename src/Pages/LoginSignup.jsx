c652237b88b96eb440507a8082cc7dfbc4e6ccea
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-06-18 20:33:22 +0600

    SignUp done

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,7 +1,22 @@
-
+import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className="loginSignup">
+      <div className="loginSignup-container">
+        <h1>Sign up</h1>
+        <div className="loginSignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login Here</span></p>
+        <div className="loginSignup-agree">
+          <input type="checkbox" name="" id=""/>
+          <p>By Contuning, I agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
